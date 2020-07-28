d06cc585a0e63cc1fe7f745374f6c28df2e314eb
Walter A Gysin <gysinwalter@gmail.com>
2020-07-28 16:30:09 -0300

    changes on style

--- src/scripts/HotelList/HotelList.js
+++ src/scripts/HotelList/HotelList.js
@@ -1,8 +1,13 @@
 class HotelList extends React.Component {
   render() {
-    const Hotels = this.props.hotelsData.map((hotel) => (
-      <Hotel key={hotel.name + hotel.country + hotel.city} hotel={hotel} />
-    ));
+    const Hotels =
+      this.props.hotelsData.length !== 0 ? (
+        this.props.hotelsData.map((hotel) => (
+          <Hotel key={hotel.name + hotel.country + hotel.city} hotel={hotel} />
+        ))
+      ) : (
+        <p>¡ Ups, Lo sentimos ! No hemos encontrado Hoteles con ese criterio</p>
+      );
     return (
       <div className="d-flex flex-wrap m-5 justify-content-around">
         {Hotels}
